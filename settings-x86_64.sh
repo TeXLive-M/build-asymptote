a0c61dccdcb13f0788525b2d426fb540f2dd334e
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2019-03-01 23:33:38 +0100

    Update settings to build on Mac OS X 10.6/x86_64

--- settings-x86_64.sh
+++ settings-x86_64.sh
@@ -1,14 +1,24 @@
-export MACOSX_DEPLOYMENT_TARGET=10.6
-export MYLDFLAGS="-L/Developer/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk/usr/lib"
-export MYSYSROOT="-isysroot /Developer/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+arch=x86_64
+targ=10.6
+sdk=10.7
 
-export CC="/usr/bin/gcc-4.2 -arch $ARCH"
-export CXX="/usr/bin/g++-4.2 -arch $ARCH"
+export CC="/opt/local/bin/clang-mp-7.0"
+export CXX="/opt/local/bin/clang++-mp-7.0 -stdlib=libc++"
 export OBJC="$CC"
 export OBJCXX="$CXX"
 
-export CFLAGS="$MYSYSROOT"
-export CXXFLAGS="$MYSYSROOT"
-export OBJCFLAGS="$MYSYSROOT"
-export OBJCXXFLAGS="$MYSYSROOT"
-export LDFLAGS="$MYLDFLAGS $MYSYSROOT"
+LIBDIR="-L/Developer/SDKs/MacOSX${sdk}.sdk/usr/lib"
+LIBDIR=""
+
+SYSROOT="-isysroot /Developer/SDKs/MacOSX${sdk}.sdk -mmacosx-version-min=${targ}"
+C_FLAGS="-Os $SYSROOT"
+L_FLAGS="-Os $SYSROOT $LIBDIR"
+
+export CFLAGS="$C_FLAGS"
+export OBJCFLAGS="$C_FLAGS"
+export CXXFLAGS="$C_FLAGS"
+export OBJCXXFLAGS="$C_FLAGS"
+export LDFLAGS="$L_FLAGS"
+
+export STRIP="strip -u -r"
+

5ad428145b58f06809431fe06584b89f50ea7b19
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2019-03-01 23:32:36 +0100

    Update versions of dependencies

--- settings.sh
+++ settings.sh
@@ -17,17 +17,17 @@ INST_PATH="${BUILD_PATH}/inst"
 
 ZIPS_PATH="${OWNPATH}/zips"
 
-FFTW_VERS=3.3.6-pl2
+FFTW_VERS=3.3.8
 FFTW_NAME="fftw-${FFTW_VERS}"
 FFTW_FILE="${FFTW_NAME}.tar.gz"
 FFTW_SRC="http://www.fftw.org/${FFTW_FILE}"
 
-GSL_VERS=2.3
+GSL_VERS=2.5
 GSL_NAME="gsl-${GSL_VERS}"
 GSL_FILE="${GSL_NAME}.tar.gz"
-GSL_SRC="http://gnu.mirror.si/gsl/${GSL_FILE}"
+GSL_SRC="ftp://ftp.gnu.org/gnu/gsl/${GSL_FILE}"
 
-READLINE_VERS=7.0
+READLINE_VERS=8.0
 READLINE_NAME="readline-${READLINE_VERS}"
 READLINE_FILE="${READLINE_NAME}.tar.gz"
 READLINE_SRC="https://ftp.gnu.org/gnu/readline/${READLINE_FILE}"

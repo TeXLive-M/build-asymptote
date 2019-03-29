e2d040c77b8bc32455e7bf2ee9cb10217516abe4
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2019-03-29 09:23:10 +0100

    Only build for x86_64

--- do_all.sh
+++ do_all.sh
@@ -21,7 +21,8 @@ esac
 
 ./do_fetch_requirements.sh
 
-for arch in x86_64 i386 ppc; do
+#for arch in x86_64 i386 ppc; do
+for arch in x86_64; do
 	./do_build_requirements.sh --arch=${arch}
 	./do_build_asy.sh --arch=${arch}
 done

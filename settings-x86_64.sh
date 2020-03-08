11d12eb92681bc942d20dabd0402a2ceb2c33441
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2020-03-08 20:55:41 +0100

    Switch to clang 9.0 for 64-bit macOS

--- settings-x86_64.sh
+++ settings-x86_64.sh
@@ -2,13 +2,12 @@ arch=x86_64
 targ=10.6
 sdk=10.6
 
-export CC="/opt/local/bin/clang-mp-7.0"
-export CXX="/opt/local/bin/clang++-mp-7.0 -stdlib=libc++"
+export CC="/opt/local/bin/clang-mp-9.0"
+export CXX="/opt/local/bin/clang++-mp-9.0 -stdlib=libc++"
 export OBJC="$CC"
 export OBJCXX="$CXX"
 
 LIBDIR="-L/Developer/SDKs/MacOSX${sdk}.sdk/usr/lib"
-LIBDIR=""
 
 SYSROOT="-isysroot /Developer/SDKs/MacOSX${sdk}.sdk -mmacosx-version-min=${targ}"
 C_FLAGS="-O3 $SYSROOT"
@@ -16,8 +15,8 @@ L_FLAGS="-O3 $SYSROOT $LIBDIR"
 
 export CFLAGS="$C_FLAGS"
 export OBJCFLAGS="$C_FLAGS"
-export CXXFLAGS="$C_FLAGS"
-export OBJCXXFLAGS="$C_FLAGS"
+export CXXFLAGS="$C_FLAGS -std=c++11"
+export OBJCXXFLAGS="$C_FLAGS -std=c++11"
 export LDFLAGS="$L_FLAGS"
 
 export STRIP="strip -u -r"

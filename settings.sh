c5996045cd4e56c1cf60a6bd2278b87c6e5f8336
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2020-03-08 19:47:55 +0100

    Update versions of dependencies

--- settings.sh
+++ settings.sh
@@ -22,7 +22,7 @@ FFTW_NAME="fftw-${FFTW_VERS}"
 FFTW_FILE="${FFTW_NAME}.tar.gz"
 FFTW_SRC="http://www.fftw.org/${FFTW_FILE}"
 
-GSL_VERS=2.5
+GSL_VERS=2.6
 GSL_NAME="gsl-${GSL_VERS}"
 GSL_FILE="${GSL_NAME}.tar.gz"
 GSL_SRC="ftp://ftp.gnu.org/gnu/gsl/${GSL_FILE}"
@@ -38,7 +38,7 @@ GLEW_NAME="glew-${GLEW_VERS}"
 GLEW_FILE="${GLEW_NAME}.tgz"
 GLEW_SRC="https://github.com/nigels-com/glew/releases/download/${GLEW_NAME}/${GLEW_FILE}"
 
-GLM_VERS=0.9.9.4
+GLM_VERS=0.9.9.7
 GLM_NAME="glm-${GLM_VERS}"
 GLM_FILE="${GLM_VERS}.tar.gz"
 GLM_SRC="https://github.com/g-truc/glm/archive/${GLM_FILE}"
